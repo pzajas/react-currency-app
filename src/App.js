df68a2f7f0d2fb094c8120b9c3a4a9f6266f63ee
Zajas Piotr <zajas.piotr@gmail.com>
2021-12-13 21:46:49 +0100

    Render the currency list on the screen

--- src/App.js
+++ src/App.js
@@ -2,20 +2,39 @@ import axios from "axios"
 import React, { useState, useEffect } from "react"
 
 import "./App.css"
+import CurrencyList from "./components/CurrencyList"
 
 const App = () => {
   const currencyApiUrl = `https://freecurrencyapi.net/api/v2/latest?apikey=86c489a0-5a0d-11ec-a1ea-9309d8ea8734&base_currency=PLN`
 
-  const [currencyList, setCurrencyList] = useState([])
+  const [everyCurrency, setEveryCurrency] = useState([])
   const [baseCurrency, setBaseCurrency] = useState(currencyApiUrl.slice(-3))
+
+  const popularCurrency = [
+    { currency: "PLN", country: "pl" },
+    { currency: "CAD", country: "ca" },
+    { currency: "USD", country: "us" },
+    { currency: "CHF", country: "sz" },
+    { currency: "RUB", country: "rs" },
+    { currency: "AUD", country: "as" },
+    { currency: "NOK", country: "no" },
+    { currency: "CZK", country: "ez" },
+  ]
 
   useEffect(() => {
     axios.get(currencyApiUrl).then((response) => {
-      setCurrencyList(response.data.data)
+      setEveryCurrency(response.data.data)
     })
   }, [])
 
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <CurrencyList
+        everyCurrency={everyCurrency}
+        popularCurrency={popularCurrency}
+      />
+    </div>
+  )
 }
 
 export default App
