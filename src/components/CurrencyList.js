a9fa7e10cc71ee5f4b46cdec977bf45d066f6392
Zajas Piotr <zajas.piotr@gmail.com>
2021-12-14 22:33:29 +0100

    Add the change base currency option.

--- src/components/CurrencyList.js
+++ src/components/CurrencyList.js
@@ -2,9 +2,16 @@ import CurrencyCard from "./CurrencyCard"
 
 import "./CurrencyList.css"
 
-const CurrencyList = ({ everyCurrency, popularCurrency, baseCurrency }) => {
+const CurrencyList = ({
+  everyCurrency,
+  popularCurrency,
+  baseCurrency,
+  currencyPrice,
+  setBaseCurrency,
+}) => {
   return (
     <div className="master">
+      <div className="title-bar">CURRENCY CONVERTER</div>
       {popularCurrency.map(({ currency, country }) => (
         <ul className="currency-list">
           <li className="currency-list-item">
@@ -13,10 +20,13 @@ const CurrencyList = ({ everyCurrency, popularCurrency, baseCurrency }) => {
               country={country}
               everyCurrency={everyCurrency}
               baseCurrency={baseCurrency}
+              currencyPrice={currencyPrice}
+              setBaseCurrency={setBaseCurrency}
             />
           </li>
         </ul>
       ))}
+      <button className="add-btn">ADD CURRENCY</button>
     </div>
   )
 }
